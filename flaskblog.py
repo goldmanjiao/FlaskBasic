5669c9476638e1beeda85d397917fe35fb9f1a4e
Goldman Jiao <goldmanjiao123456@gmail.com>
2022-10-26 00:08:22 +0100

    Initial Commit

---
+++ flaskblog.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+posts = [
+
+    {
+        'author': 'Corey',
+        'title' : 'Blog Post1'
+
+    }
+]
+
+
+@app.route("/")
+
+@app.route("/home")
+def home():
+    return render_template("home.html", posts = posts)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
